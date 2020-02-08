0b021dfc6ca4bbc63edbc6f139f91273f74ae32d
rudresh thakur <rudresh04thakur@gmail.com>
2020-02-08 14:32:50 +0530

    Update of 08-02-2020

---
+++ react_api/register.php
@@ -0,0 +1,12 @@
+<?php
+    echo "Hi this is from php";
+    // $data = json_decode(file_get_contents('php://input'));
+    // if($data->email!=""){
+    // $con = mysqli_connect("localhost","root","","10_30_WEEKEND");
+    // $str = "INSERT INTO users (name, email, mobile, password ) VALUES ('$data->name','$data->email','$data->mobile','$data->password')";
+    // $result = mysqli_query($con,$str) or die(json_encode(["msg"=>"Register Unsuccessful","error"=>mysqli_error($con),"class"=>"danger"]));
+    // echo json_encode(["msg"=>"Register Successful","error"=>"","class"=>"success"]);
+    // }else{
+    //    echo json_encode(["msg"=>"Register Unsuccessful","error"=>"Empty Request","class"=>"danger"]);
+    // }
+?>
